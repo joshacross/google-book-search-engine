44196c516bba3812063dcdce73cb9a04744b0d20
Killa J <joshuaalexandercross@gmail.com>
2021-07-23 22:32:24 -0500

    fix: refactor hard coded connection string to Heroku config var

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+
+mongoose.connect(uri || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const URI = process.env.MONGODB_URI
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
 
 app.use(routes);
 
